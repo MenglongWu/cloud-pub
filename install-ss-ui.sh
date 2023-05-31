f0d1fc00e57b1aaa4d1f8ac62e3fe92307c45836
MenglongWu <MenglongWoo@aliyun.com>
2023-05-31 13:52:25 +0900

    update

--- install-ss-ui.sh
+++ install-ss-ui.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-apt-get update
+# apt-get update
+
 apt-get install net-tools
 apt-get install iptables
 apt-get install mosquitto
 apt-get install libmosquitto1
 apt-get install shadowsocks
 
+wget -N https://raw.githubusercontent.com/MenglongWu/cloud-pub/master/deb/ss-ui.deb 
+dpkg -i ss-ui.deb
