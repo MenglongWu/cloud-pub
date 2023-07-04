9d68414f738bfbc9a7df94ec9b2d7c6f80d1edd9
Menglong Woo <MenglongWoo@aliyun.com>
2023-07-04 18:29:09 -0400

    s

--- install-ss-ui.sh
+++ install-ss-ui.sh
@@ -3,12 +3,12 @@
 # apt-get update
 # echo "deb https://mirrors.aliyun.com/ubuntu/ focal main multiverse restricted universe" > /etc/apt/sources.list.d/aliyun.list
 
-apt-get install net-tools
-apt-get install iptables
-apt-get install mosquitto
-apt-get install libmosquitto1
-apt-get install shadowsocks-libev
-apt-get install rsyslog
+apt-get -y  install net-tools
+apt-get -y  install iptables
+apt-get -y  install mosquitto
+apt-get -y  install libmosquitto1
+apt-get -y  install shadowsocks-libev
+apt-get -y  install rsyslog
 
 echo -e "\nkern.warning /var/log/iptables.log" >> /etc/rsyslog.d/50-default.conf
 
@@ -18,7 +18,7 @@ dpkg -i ss-ui.deb
 
 ################################################
 
-apt-get install nginx
-apt-get install python3-pip
+apt-get -y install nginx
+apt-get -y install python3-pip
 pip3 install Flask
 
