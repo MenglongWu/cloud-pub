99cd2071aee7804849d2dd1e333259cdae14f1b0
Menglong Wu <marlonwu@126.com>
2025-11-26 19:17:56 +0800

    ss-ui
    Signed-off-by: Menglong Wu <marlonwu@126.com>

--- install-acme.sh
+++ install-acme.sh
@@ -1,7 +1,7 @@
 export CF_Key='d39008d32a8f73c0bb611aab77404e4a9b4ed'
 export CF_Email='menglongwoo@aliyun.com'
 
-wget -N https://raw.githubusercontent.com/MenglongWu/cloud-pub/master/bin/acme-ui.sh > /usr/local/bin/acme-ui.sh
+wget -N https://raw.githubusercontent.com/MenglongWu/cloud-pub/refs/heads/master/bin/acme-ui.sh -O /usr/local/bin/acme-ui.sh
 
 chmod +x /usr/local/bin/acme-ui.sh
 
